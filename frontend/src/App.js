42d531c3e5d13e5bc0693e248649bfb69c2e1206
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-15 15:14:13 -0500

    Setup file structure for styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import ProfileContainer from './profile/profile_container';
 import TweetComposeContainer from './tweets/tweet_compose_container';
-
+// import "./stylesheets/application.scss";
 
 const App = () => (
   <div>
