bf74beb8c2bb380e8f43a57f2d361e037ce90d9d
mwilson <mjwilsonmd@gmail.com>
2021-11-15 14:58:07 -0500

    create route for document index

--- routes/api/documents.js
+++ routes/api/documents.js
@@ -3,24 +3,24 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
-const Tweet = require('../../models/Tweet');
-const validateTweetInput = require('../../validation/tweets');
+const Document = require('../../models/Document');
+
 
 router.get('/', (req, res) => {
-    Tweet.find()
+    Document.find()
         .sort({ date: -1 })
-        .then(tweets => res.json(tweets))
-        .catch(err => res.status(404).json({ notweetsfound: 'No tweets found' }));
+        .then(documents => res.json(documents))
+        .catch(err => res.status(404).json({ noDocumentsFound: 'No documents found' }));
 });
 
-router.get('/user/:user_id', (req, res) => {
-    Tweet.find({user: req.params.user_id})
-        .then(tweets => res.json(tweets))
-        .catch(err =>
-            res.status(404).json({ notweetsfound: 'No tweets found from that user' }
-        )
-    );
-});
+// router.get('/user/:user_id', (req, res) => {
+//     Tweet.find({user: req.params.user_id})
+//         .then(tweets => res.json(tweets))
+//         .catch(err =>
+//             res.status(404).json({ notweetsfound: 'No tweets found from that user' }
+//         )
+//     );
+// });
 
 router.get('/:id', (req, res) => {
     Tweet.findById(req.params.id)
