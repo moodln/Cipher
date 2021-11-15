39753d69f422a9070bdac254e2b9196e57850d38
mwilson <mjwilsonmd@gmail.com>
2021-11-15 09:10:39 -0500

    change port to 5001

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World"));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
