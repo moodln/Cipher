a77da1bb992b2ad8dd274e1d195f79249b541d59
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-16 10:56:29 -0500

    Comment out tweet box and tweet compose

--- frontend/src/components/problem_index/tweet_compose_container.js
+++ frontend/src/components/problem_index/tweet_compose_container.js
@@ -1,18 +1,18 @@
-import { connect } from 'react-redux';
-import { composeTweet } from '../../actions/tweet_actions';
-import TweetCompose from './tweet_compose';
+// import { connect } from 'react-redux';
+// import { composeTweet } from '../../actions/tweet_actions';
+// import TweetCompose from './tweet_compose';
 
-const mapStateToProps = (state) => {
-  return {
-    currentUser: state.session.user,
-    newTweet: state.tweets.new
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     currentUser: state.session.user,
+//     newTweet: state.tweets.new
+//   };
+// };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    composeTweet: data => dispatch(composeTweet(data))
-  };
-};
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     composeTweet: data => dispatch(composeTweet(data))
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(TweetCompose);
+// export default connect(mapStateToProps, mapDispatchToProps)(TweetCompose);
