2ef99e98a5da996be50a033220e50361db3a4e04
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-16 16:07:01 -0500

    Rename group show to document show

---
+++ frontend/src/components/document_show/document_show.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class DocumentShow extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>A GROUP</h1>
+            </div>
+        )
+    }
+}
+
+export default DocumentShow;
