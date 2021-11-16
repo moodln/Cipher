81c639f33ee25e2c2084910ea8f545ea11c4a182
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-16 10:54:29 -0500

    Add route to group show container

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,11 +4,12 @@ import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
 import ProblemIndexContainer from './problem_index/problem_index_container';
+import GroupShowContainer from './group_show/group_show_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import ProfileContainer from './profile/profile_container';
-import TweetComposeContainer from './problem_index/tweet_compose_container';
+// import TweetComposeContainer from './problem_index/tweet_compose_container';
 
 
 const App = () => (
@@ -20,8 +21,9 @@ const App = () => (
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
 
       <ProtectedRoute exact path="/problems" component={ProblemIndexContainer} />
+      <ProtectedRoute exact path="/groups/:groupId" component={GroupShowContainer} />
       <ProtectedRoute exact path="/profile" component={ProfileContainer} />
-      <ProtectedRoute exact path="/new_tweet" component={TweetComposeContainer} />
+      {/* <ProtectedRoute exact path="/new_tweet" component={TweetComposeContainer} /> */}
     </Switch>
   </div>
 );
