a7e667dfd95a3f9336a7ef7ad6c769ade8e855e4
Arebiter <pasandharmasena@gmail.com>
2021-11-16 15:37:07 -0500

    Start organizing problems index

--- frontend/src/components/problem_index/problem_index.js
+++ frontend/src/components/problem_index/problem_index.js
@@ -9,9 +9,31 @@ class ProblemIndex extends React.Component {
     render() {
         return (
             <div className="problem-index container">
-                <h1>ALL PROBLEMS</h1>
-                <ul className="problem-index-section">
-                {/* {
+                <section className="problem-index-header">
+                    <div className="problem-index-header-div">
+                        <div className="problem-intro">
+                            <h1>Solve problems at your own page or with friends</h1>
+                        </div>
+                        <section className="problem-index-instructions">
+                            <div className="problem-summary-item">
+                                <h2 className="problem-summary-item-header">Code</h2>
+                                <h4 className="problem-summary-item-subtitle">Choose from a selection of 100+ practice problems</h4>
+                            </div>
+                            <div className="problem-summary-item">
+                                <h2 className="problem-summary-item-header">Collaborate</h2>
+                                <h4 className="problem-summary-item-subtitle">Build strong connections with other developers using our video chat feature</h4>
+                            </div>
+                            <div className="problem-summary-item">
+                                <h2 className="problem-summary-item-header">Cultivate</h2>
+                                <h4 className="problem-summary-item-subtitle">Practice your algorithmic skills in a collaborative coding environment</h4>
+                            </div>
+                        </section>
+                    </div>
+                </section>
+                <section className="problem-index-problems-section">
+                    <h1>ALL PROBLEMS</h1>
+                    <ul className="problem-index-problems-list">
+                        {/* {
                     this.props.problems.map(problem => (
                         // li onClick should create document and group,
                         // then this.props.history.push(`/groups/${groupId}`) to group show page
@@ -20,9 +42,8 @@ class ProblemIndex extends React.Component {
                         </li>
                     ))
                 } */}
-                </ul>
-
-
+                    </ul>
+                </section>
             </div>
         )
     }
