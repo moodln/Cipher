724bbe92e48cbe9180def3b4fdd47fcf9b150622
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-15 22:41:41 -0500

    Change tweets route to problems route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,12 +3,12 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
-import TweetsContainer from './tweets/tweets_container';
+import ProblemIndexContainer from './problem_index/problem_index_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import ProfileContainer from './profile/profile_container';
-import TweetComposeContainer from './tweets/tweet_compose_container';
+import TweetComposeContainer from './problem_index/tweet_compose_container';
 
 
 const App = () => (
@@ -19,7 +19,7 @@ const App = () => (
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
 
-      <ProtectedRoute exact path="/tweets" component={TweetsContainer} />
+      <ProtectedRoute exact path="/problems" component={ProblemIndexContainer} />
       <ProtectedRoute exact path="/profile" component={ProfileContainer} />
       <ProtectedRoute exact path="/new_tweet" component={TweetComposeContainer} />
     </Switch>
