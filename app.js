1a85fde053246a1b55f1fcaf2b7c928b4f89a47a
mwilson <mjwilsonmd@gmail.com>
2021-11-17 18:04:07 -0500

    working on signaling

--- app.js
+++ app.js
@@ -38,41 +38,55 @@ app.use("/api/documents", documents);
 app.use("/api/problems", problems);
 
 const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer);
-
+const io = require('socket.io')(httpServer, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST' ]
+    } 
+})
+let connectedUsers = [];
 io.on("connection", socket => {
   console.log("User online");
+  connectedUsers.push(socket.id);
+  socket.broadcast.emit('currentUsers', {userIds: connectedUsers});
 
   // if server receives event with name "canvas-data", 
   // message will be broadcast to all other connected users
   socket.on("canvas-data", data => {
     socket.broadcast.emit("canvas-data", data);
   })
-})
+
+  // socket.on('join', (data) => {
+  //   socket.join(data.username)
+  // })
+
+  // grab socked id and apply it to client-side
+  // socket.emit('localId', socket.id)
+
+  // socket.on('call', (data) => {
+  //   // the person we want to call, userToCall is passed in from frontend
+  //   io.to(data.toUsername).emit('call', {
+  //       signal: data.signalData, 
+  //       from: data.from, 
+  //       name: data.name})
+  // })
 
 
-// connect two people through socketIDs
-io.on('connection', (socket) => {
-  socket.emit('me', socket.id)
+  // socket.on('answer', (data) => { 
+  //   io.to(data.to).emit('callAccepted', data.signal)
+  // })
 
   socket.on('disconnect', () => {
-    socket.broadcast.emti('callEnded')
-  })
-
-  socket.on('callUser', (data) => {
-    // the person we want to call, userToCall is passed in from frontend
-    io.to(data.userToCall).emit('callUser', {
-        signal: data.signalData, 
-        from: data.from, 
-        name: data.name})
-  })
-
-  socket.on('answerCall', (data) => { 
-    io.to(data.to).emit('callAccepted'), data.signal
+    console.log('user disconnected')
+    connectedUsers = connectedUsers.filter(user => user !==socket.id);
+    socket.broadcast.emit('currentUsers', {userIds: connectedUsers});
+    socket.broadcast.emit('callEnded')
   })
 })
 
-const port = process.env.PORT || 3300;
+
+
+// const port = process.env.PORT || 3300;
 httpServer.listen(process.env.PORT || 3300, function () {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
--- frontend/src/components/video_stream/video.js
+++ frontend/src/components/video_stream/video.js
@@ -1,155 +1,265 @@
-import React, { useEffect, useRef, useState} from 'react';
-// use to copy Id
-// import { CopyToClipboard } from 'react-copy-to-clipboard';
+// import React, { useEffect, useRef, useState} from 'react';
+// // use to copy Id
+// // import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 
+// import io from 'socket.io-client';
+
+
+
+
+// function Video() {
+//     // peer to peer connection
+//     const Peer = require('simple-peer');
+//     const socket = io.connect('http://localhost:3300')
+//     // construct initial state
+//     const [ myId, setMyId ] = useState('')
+//     const [ stream, setStream ] = useState()
+//     const [ activeSockets, setActiveSockets ] = useState([])
+//     const [ receivingCall, setReceivingCall ] = useState(false)
+//     const [ caller, setCaller ] = useState('')
+//     const [ callerSignal, setCallerSignal ] = useState()
+//     const [ callAccepted, setCallAccepted ] = useState(false)
+//     const [ idToCall, setIdToCall ] = useState('')
+//     const [ callEnded, setCallEnded ] = useState(false)
+//     const [ name, setName ] = useState('')
+
+//     // we use useRef so that we can attach to html element 'ref' later
+//     // reference video
+//     const myVideo = useRef()
+//     const oppositeVideo = useRef()
+//     // disconnect when call ends
+//     const connectionRef = useRef()
+
+//     // set everything on initial load
+//     useEffect(() => {
+//         // will ask for permission to use webcam, then assign the video stream to myVideo to stream locally
+//         navigator.mediaDevices.getUserMedia({ video: true, audio: true})
+//             .then(stream => {
+//                 console.log(stream)
+//                 // add stream to state
+//                 setStream(stream)
+//                 // set reference to video to stream coming in from webcam
+//                 myVideo.current.srcObject = stream
+//             })
+
+//             socket.on('myId', (id) => {
+//                 // grab id from backend and set it to frontend id
+//                 setMyId(id)
+//             })
+
+//             socket.on('callUser', (data) => {
+//                 setReceivingCall(true)
+//                 setCaller(data.from)
+//                 setName(data.name)
+//                 setCallerSignal(data.signal)
+//             })
+//     }, [])
+
+//     // create a new peer
+//     const callUser = (id) => {
+//         const peer = new Peer({
+//             initiator: true,
+//             trickle: false, 
+//             stream: stream
+//         })
+
+//         peer.on('signal', (data) => {
+//             socket.emit('callUser', {
+//                 userToCall: id, 
+//                 signalData: data, 
+//                 from: myId,
+//                 name: name
+//             })
+//         })
+
+//         peer.on('stream', (stream) => {
+//             console.log(stream)
+//             oppositeVideo.current.srcObject = stream 
+//         })
+
+//         socket.on('callaccepted', (signal) => {
+//             setCallAccepted(true)
+//             peer.signal(signal)
+//         })
+
+//         connectionRef.current = peer
+//     }
+
+//     const answerCall = () => {
+//         setCallAccepted(true)
+//         const peer = new Peer({
+//             initiator: false, 
+//             trickle: false, 
+//             stream: stream
+//         })
+
+//         peer.on('signal', (data) => {
+//             socket.emit('answerCall', {signal: data, to: caller})
+//         })
+
+//         peer.on('stream', (stream) => {
+//             console.log(stream)
+//             console.log(oppositeVideo)
+//             console.log(myVideo)
+//             oppositeVideo.current.srcObject = stream
+//         })
+
+//         peer.signal(callerSignal)
+
+//         connectionRef.current = peer
+//     }
+
+//     const leaveCall = () => {
+//         setCallEnded(true)
+//         connectionRef.current.destroy()
+//     }
+
+//     return (
+//         <div className='video-container'>
+//             <div className='video' >
+//                 {/* terniary for return statement */}
+//                 {stream && <video playsInline muted ref={myVideo} autoPlay className="local-video" id="local-video" />}
+//             </div>
+//             <div className='video'>
+//                 {callAccepted && !callEnded ? 
+//                 <video playsInline ref={oppositeVideo} autoPlay className="remote-video" id="remote-video"/> : null }
+//             </div>
+//             <input type="text" 
+//                 value={name} 
+//                 style={{background: 'white', color: 'black'}}
+//                 onChange={e => setName(e.target.value)} />
+//             <input type="text" 
+//                 value={idToCall} 
+//                 style={{background: 'white', color: 'black'}}
+//                 onChange={e => setIdToCall(e.target.value)} />
+//             <textarea name={myId} value={myId}id="" cols="30" rows="10"></textarea>
+//             <div>
+//                 {callAccepted && !callEnded ? (
+//                     <button onClick={leaveCall}>end call</button>
+//                 ) : (
+//                     <button onClick={() => callUser(idToCall)}>call</button>
+//                 )}
+//                 {idToCall}
+//             </div>
+//             <div>
+//                 {receivingCall && !callAccepted ? (
+//                     <div>
+//                         <h1>{name} is calling...</h1>
+//                         <button onClick={answerCall}>answer</button>
+//                     </div>
+//                 ) : null}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Video;
+
+
+
+import React, { useState, useRef, useEffect } from 'react';
+import adapter from 'webrtc-adapter';
 import io from 'socket.io-client';
-
-const socket = io.connect('http://localhost:3300')
 
 
 function Video() {
-    // peer to peer connection
+
+    const socket = io.connect('http://localhost:3300')
     const Peer = require('simple-peer');
+    const [localId, setLocalId] = useState();
+    const [signal, setSignal] = useState();
+    const [caller, setCaller] = useState('');
+    const [receivingCall, setReceivingCall] = useState(false);
+    const [name, setName] = useState();
+    const localVideo = useRef();
+    const remoteVideo = useRef();
 
-    // construct initial state
-    const [ myId, setMyId ] = useState('')
-    const [ stream, setStream ] = useState()
-    const [ remoteStream, setRemoteStream ] = useState()
-    const [ receivingCall, setReceivingCall ] = useState(false)
-    const [ caller, setCaller ] = useState('')
-    const [ callerSignal, setCallerSignal ] = useState()
-    const [ callAccepted, setCallAccepted ] = useState(false)
-    const [ idToCall, setIdToCall ] = useState('')
-    const [ callEnded, setCallEnded ] = useState(false)
-    const [ name, setName ] = useState('')
+    let selectedUser;
 
-    // we use useRef so that we can attach to html element 'ref' later
-    // reference video
-    const myVideo = useRef()
-    const oppositeVideo = useRef()
-    // disconnect when call ends
-    const connectionRef = useRef()
+    const onUpdateUserList = ({ userIds }) => {
+    const usersList = document.querySelector('#usersList');
+    const usersToDisplay = userIds.filter(id => id !== socket.id);
+    usersList.innerHTML = '';
+    
+    usersToDisplay.forEach(user => {
+        const userItem = document.createElement('div');
+        userItem.innerHTML = user;
+        userItem.className = 'user-item';
+        
+        userItem.addEventListener('click', () => {
+        selectedUser = user;
+        });
+        usersList.appendChild(userItem);
+    });
+    };
 
-    // set everything on initial load
+    socket.on('currentUsers', onUpdateUserList);
+
+    const peerToPeerConnection = () => {
+        return new RTCPeerConnection({
+            iceServers: [
+                {
+                    urls: 'stun:stun.stunprotocol.org'
+                }
+            ]
+        });
+    };
+    const peer = peerToPeerConnection();
+    
+
     useEffect(() => {
-        // will ask for permission to use webcam, then return the video stream in an object
-        navigator.mediaDevices.getUserMedia({ video: true, audio: true})
+        navigator.mediaDevices.getUserMedia(
+            {
+                audio: {
+                    echoCancellation: true
+                }, 
+                video: true
+            })
             .then(stream => {
-                console.log(stream)
-                // add stream to state
-                setStream(stream)
-                // set reference to video to stream coming in from webcam
-                myVideo.current.srcObject = stream
+                localVideo.current.srcObject = stream;
+                stream.getTracks().forEach(track => peer.addTrack(track, stream))
+            })
+            .catch(err => {
+                console.log(err)
             })
 
-            socket.on('myId', (id) => {
+            socket.on('connect', () => {
+                socket.emit('join', {
+                    // x
+                })
+            })
+
+            socket.on('localId', (id) => {
                 // grab id from backend and set it to frontend id
-                setMyId(id)
+                setLocalId(id)
             })
 
-            socket.on('callUser', (data) => {
-                setReceivingCall(true)
+            socket.on('call', (data) => {
+                setSignal(data.signal)
                 setCaller(data.from)
                 setName(data.name)
-                setCallerSignal(data.signal)
+                setReceivingCall(true)
             })
+
+
     }, [])
-
-    // create a new peer
-    const callUser = (id) => {
-        const peer = new Peer({
-            initiator: true,
-            trickle: false, 
-            stream: stream
-        })
-
-        peer.on('signal', (data) => {
-            socket.emit('callUser', {
-                userToCall: id, 
-                signalData: data, 
-                from: myId,
-                name: name
-            })
-        })
-
-        // peer.on('stream', (stream) => {
-        //     console.log(stream)
-        //     oppositeVideo.current.srcObject = stream 
-        // })
-
-        socket.on('callaccepted', (signal) => {
-            setCallAccepted(true)
-            peer.signal(signal)
-        })
-
-        connectionRef.current = peer
+    const otherVideo = (data) => {
+        const otherUser = new Peer();
     }
-
-    const answerCall = () => {
-        setCallAccepted(true)
-        const peer = new Peer({
-            initiator: false, 
-            trickle: false, 
-            stream: stream
-        })
-
-        peer.on('signal', (data) => {
-            socket.emit('answerCall', {signal: data, to: caller})
-        })
-
-        peer.on('stream', (stream) => {
-            console.log(stream)
-            oppositeVideo.current.srcObject = stream
-        })
-
-        peer.signal(callerSignal)
-
-        connectionRef.current = peer
-    }
-
-    const leaveCall = () => {
-        setCallEnded(true)
-        connectionRef.current.destroy()
-    }
-
+    
+    
     return (
-        <div className='video-container'>
-            <div className='video' >
-                {/* terniary for return statement */}
-                {stream && <video playsInline muted ref={myVideo} autoPlay  />}
-            </div>
-            <div className='video'>
-                {callAccepted && !callEnded ? 
-                <video playsInline muted ref={oppositeVideo} autoPlay /> : null }
-            </div>
-            <input type="text" 
-                value={name} 
-                style={{background: 'white', color: 'black'}}
-                onChange={e => setName(e.currentTarget.value)} />
-            <input type="text" 
-                value={idToCall} 
-                style={{background: 'white', color: 'black'}}
-                onChange={e => setIdToCall(e.currentTarget.value)} />
-            <textarea name={myId} value={myId}id="" cols="30" rows="10"></textarea>
+        <div className="video-container">
+            <div className='userId'>My user id: {socket.id}</div>
+            <video className='localVideo' playsInline muted ref={localVideo} autoPlay ></video>
+            <video className='remoteVideo' playsInline ref={remoteVideo} autoPlay ></video>
             <div>
-                {callAccepted && !callEnded ? (
-                    <button onClick={leaveCall}>end call</button>
-                ) : (
-                    <button onClick={() => callUser(idToCall)}>call</button>
-                )}
-                {idToCall}
-            </div>
-            <div>
-                {receivingCall && !callAccepted ? (
-                    <div>
-                        <h1>{name} is calling...</h1>
-                        <button onClick={answerCall}>answer</button>
-                    </div>
-                ) : null}
+                <button className='call'>call</button>
             </div>
         </div>
     )
+    
 }
 
 export default Video;
