04b28b2b1b030b260d004e83bab4c05f883a87f8
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-17 17:23:43 -0500

    Add github links and a tags to footer names

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -6,10 +6,22 @@ class Footer extends React.Component {
         return (
             <section className="footer-section container">
                 <div className="footer-names">
-                    <h2>Rebecca Foster</h2>
-                    <h2>Madeline Wilson</h2>
-                    <h2>Maria Vaghani</h2>
-                    <h2>Pasan Dharmasena</h2>
+                    <a href="https://github.com/rebeccamrfoster"
+                        target="_blank" rel="noopener noreferrer">
+                        <h2>Rebecca Foster</h2>
+                    </a>
+                    <a href="https://github.com/moodln"
+                        target="_blank" rel="noopener noreferrer">
+                        <h2>Madeline Wilson</h2>
+                    </a>
+                    <a href="https://github.com/mariavaghani"
+                        target="_blank" rel="noopener noreferrer">
+                        <h2>Maria Vaghani</h2>
+                    </a>
+                    <a href="https://github.com/Arebiter"
+                        target="_blank" rel="noopener noreferrer">
+                        <h2>Pasan Dharmasena</h2>
+                    </a>
                 </div>
                 <div className="footer-links">
                     <a href="#">Cipher GitHub</a>
