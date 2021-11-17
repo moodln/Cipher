918c13497b764d46b70462cdfd7cff19f9ee043f
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-17 15:45:23 -0500

    Rename signup form container to register

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
 import ProblemIndexContainer from './problem_index/problem_index_container';
 import DocumentShowContainer from './document_show/document_show_container';
 // import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
-import SignupFormContainer from './session/signup_form_container';
+import RegisterFormContainer from './session/register_form_container';
 import DashboardContainer from './profile/dashboard';
 // import TweetComposeContainer from './problem_index/tweet_compose_container';
 import Splash from "./splash/splash";
 import Footer from "./footer/footer";
+import GroupShowDummy from "./group_show_dummy";
 
 
 const App = () => (
   <div className="page-container">
     <div className="main-content">
       <NavBarContainer />
+      {/* <GroupShowDummy /> */}
       <Switch>
-        <AuthRoute exact path="/" component={Splash} />
+        <Route exact path="/" component={Splash} />
         <AuthRoute exact path="/login" component={LoginFormContainer} />
-        <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        <AuthRoute exact path="/register" component={RegisterFormContainer} />
 
         <ProtectedRoute exact path="/problems" component={ProblemIndexContainer} />
         {/* <ProtectedRoute exact path="/groups/:groupId" component={GroupShowContainer} /> */}
