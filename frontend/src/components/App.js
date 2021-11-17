925b83f66d4e01178ecbdd167a6fe6dd90603d10
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-17 16:54:17 -0500

    Revert splash route to auth route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
 import ProblemIndexContainer from './problem_index/problem_index_container';
@@ -18,7 +18,7 @@ const App = () => (
     <div className="main-content">
       <NavBarContainer />
       <Switch>
-        <Route exact path="/" component={Splash} />
+        <AuthRoute exact path="/" component={Splash} />
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/register" component={RegisterFormContainer} />
 
