e2503402352c63ea640fbadcb740cd3c4dde0650
Arebiter <pasandharmasena@gmail.com>
2021-11-18 11:08:12 -0500

    Add user handles, push to main

--- frontend/src/components/group_show/group_show.js
+++ frontend/src/components/group_show/group_show.js
@@ -10,44 +10,44 @@ import InviteButtonContainer from '../ui_components/invite_button'
 class GroupShow extends Component {
 
   componentDidMount() {
-    
+
     this.props.fetchGroup(this.props.match.params.groupId)
   }
-  
+
   render() {
     if (!this.props.group) return null;
     if (!this.props.problem) return null;
-    const {group, problem} = this.props;
+    const { group, problem } = this.props;
     return (
       <div>
-        <InviteButtonContainer groupId={this.props.group._id} participants={group.users} invitedUsers={this.props.invitedUsers.allIds}/>
+        <InviteButtonContainer groupId={this.props.group._id} participants={group.users} invitedUsers={this.props.invitedUsers.allIds} />
         <h1>{this.props.problem.body}</h1>
         <h1>Participants:</h1>
         <ul>
-         {
-         this.props.participants.map(user => {
-          return (
-            <li key={user["_id"]}>
-              {user.email}
-            </li>
-          )
-        })
-        }
-       </ul>
-       <h1>Invited Users:</h1>
-       <ul>
-         {
-         Object.values(this.props.invitedUsers.byId).map(user => {
-          return (
-            <li key={user["_id"]} 
-            
-            >
-              {user.email}
-            </li>
-          )
-        })
-        }
-       </ul>
+          {
+            this.props.participants.map(user => {
+              return (
+                <li key={user["_id"]}>
+                  {user.email}
+                </li>
+              )
+            })
+          }
+        </ul>
+        <h1>Invited Users:</h1>
+        <ul>
+          {
+            Object.values(this.props.invitedUsers.byId).map(user => {
+              return (
+                <li key={user["_id"]}
+
+                >
+                  {user.email}
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
     )
   }
@@ -58,14 +58,15 @@ const mapStateToProps = (state, ownProps) => {
   if (Object.values(state.entities.groups.byId).length === 0) return {};
   if (Object.values(state.entities.problems.byId).length === 0) return {};
   // console.log(`state.entities.groups.byId: `, state.entities.groups.byId[groupId].document.problem);
-  
+
   const problemId = state.entities.groups.byId[groupId].document.problem;
   return {
-  group: state.entities.groups.byId[groupId],
-  problem: state.entities.problems.byId[problemId],
-  participants: selectGroupParticipants(state.entities.users.byId, state.entities.groups.byId[groupId].users),
-  invitedUsers: selectUsersInvitedToGroup(state.entities.users.byId, state.entities.invites.byId, groupId)
-}}
+    group: state.entities.groups.byId[groupId],
+    problem: state.entities.problems.byId[problemId],
+    participants: selectGroupParticipants(state.entities.users.byId, state.entities.groups.byId[groupId].users),
+    invitedUsers: selectUsersInvitedToGroup(state.entities.users.byId, state.entities.invites.byId, groupId)
+  }
+}
 
 const mapDispatchToProps = (dispatch) => ({
   fetchGroup: (groupId) => dispatch(fetchGroup(groupId))
