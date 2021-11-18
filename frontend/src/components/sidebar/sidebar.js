2b69552b29adbad55062da66e9bdf3b44317482e
mwilson <mjwilsonmd@gmail.com>
2021-11-18 15:11:39 -0500

    merge

--- frontend/src/components/sidebar/sidebar.js
+++ frontend/src/components/sidebar/sidebar.js
@@ -61,7 +61,7 @@ class Sidebar extends React.Component {
                             }
                         </div>
                     </div>
-                    <div className="sidebar-section">
+                    {/* <div className="sidebar-section">
                         <button className="sidebar-button">
                             <svg className="problems-svg bi bi-files" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                                 <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z" />
@@ -80,7 +80,7 @@ class Sidebar extends React.Component {
                                 })
                             }
                         </div>
-                    </div>
+                    </div> */}
                     <div className="sidebar-section">
                         <button className="sidebar-button">
                             <svg className="problems-svg bi bi-people" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
