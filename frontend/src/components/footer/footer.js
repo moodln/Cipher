3cd4b184e524e4f2e3e416ec80bba926df5d22c4
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-17 23:12:06 -0500

    Refactor footer structure, class names, and content

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -1,11 +1,70 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import FooterAuthorsInfo from "./footer_authors_info";
+import portraitRebecca from "../../assets/images/portrait_rebecca.jpg";
+import portraitPasan from "../../assets/images/portrait_pasan.jpeg";
 
 class Footer extends React.Component {
     render() {
         return (
-            <section className="footer-section container">
-                <div className="footer-names">
+            <section className="footer-section">
+                <div className="footer-authors-container">
+                    <div className="footer-authors">
+                        <div className="footer-portraits">
+                            <img src={portraitPasan} />
+                        </div>
+                        <FooterAuthorsInfo name={"Madeline Wilson"}/>
+                    </div>
+
+                    <div className="footer-authors">
+                        <div className="footer-portraits">
+                            <img src={portraitRebecca} />
+                        </div>
+                        <FooterAuthorsInfo name={"Maria Vaghani"} />
+                    </div>
+
+                    <div className="footer-authors">
+                        <div className="footer-portraits">
+                            <img src={portraitPasan} />
+                        </div>
+                        <FooterAuthorsInfo name={"Pasan Dharmasena"} />
+                    </div>
+
+                    <div className="footer-authors">
+                        <div className="footer-portraits">
+                            <img src={portraitRebecca} />
+                        </div>
+                        <FooterAuthorsInfo name={"Rebecca Foster"} />
+                    </div>
+                </div>
+
+                <div className="footer-summary">
+                    <p>
+                        We are four software engineers who are passionate about 
+                        coding, collaboration, and design.
+                    </p>
+                    {/* <br></br> */}
+                    {/* <p> 
+                        Cipher was inspired by our desire for a coding 
+                        environment in which you can practice the driver/navigator
+                        roles from a shared code editor and over videochat from
+                        the same application.
+                    </p> */}
+                </div>
+
+                <h4>&#169; 2021 Cipher</h4>
+            </section>
+
+
+
+
+
+
+
+
+            // <section className="footer-section container">
+        
+                /* <div className="footer-names">
                     <a href="https://github.com/rebeccamrfoster"
                         target="_blank" rel="noopener noreferrer">
                         <h2>Rebecca Foster</h2>
@@ -29,8 +88,8 @@ class Footer extends React.Component {
                 <div className="footer-summary">
                     <p>We came up with this project because...</p>
                     <p>Some other stuff we have to say</p>
-                </div>
-            </section>
+                </div> */
+            /* </section> */
         )
     }
 
