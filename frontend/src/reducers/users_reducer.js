cc931123d8f1938184c808effb6bfcb567baf27d
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-22 16:05:06 -0500

    Clean code and implement ES6 syntax

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -2,36 +2,34 @@ import { RECEIVE_GROUP, RECEIVE_USER_GROUPS } from "../actions/group_actions";
 import { RECEIVE_USERS_SEARCH } from "../actions/user_actions";
 
 const _nullState = {
-  byId: {},
-  allIds: []
-}
-
-export const UsersReducer = (state = _nullState, action) => {
-
-  Object.freeze(state);
-  const nextState = Object.assign({}, state);
-
-  switch (action.type) {
-    case RECEIVE_USER_GROUPS:
-      nextState.byId = action.groupsCollection.usersById;
-      nextState.allIds = action.groupsCollection.allUsersId;
-      return nextState;
-    case RECEIVE_USERS_SEARCH:
-      Object.values(action.usersCollection.usersById).forEach(user => {
-        nextState.byId[user._id] = user;
-        
-      });
-      nextState.allIds = nextState.allIds.concat(action.usersCollection.allUsersId);
-      return nextState;
-      
-    case RECEIVE_GROUP:
-      Object.values(action.groupCollection.usersById).forEach(user => {
-        nextState.byId[user._id] = user;
-        
-      });
-      nextState.allIds = nextState.allIds.concat(action.groupCollection.allUsersId);
-      return nextState;
-    default:
-      return state;
-  }
+    byId: {},
+    allIds: []
 };
+
+const UsersReducer = (state = _nullState, action) => {
+    Object.freeze(state);
+    const nextState = Object.assign({}, state);
+
+    switch (action.type) {
+        case RECEIVE_USER_GROUPS:
+            nextState.byId = action.groupsCollection.usersById;
+            nextState.allIds = action.groupsCollection.allUsersId;
+            return nextState;
+        case RECEIVE_USERS_SEARCH:
+            Object.values(action.usersCollection.usersById).forEach(user => {
+                nextState.byId[user._id] = user;
+            });
+            nextState.allIds = nextState.allIds.concat(action.usersCollection.allUsersId);
+            return nextState;
+        case RECEIVE_GROUP:
+            Object.values(action.groupCollection.usersById).forEach(user => {
+                nextState.byId[user._id] = user;
+            });
+            nextState.allIds = nextState.allIds.concat(action.groupCollection.allUsersId);
+            return nextState;
+        default:
+            return state;
+    }
+};
+
+export default UsersReducer;
