ce8ebb9cfccbfae63452b54583fc4eb80e5a389f
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-22 16:17:31 -0500

    Change single quotes to double quotes for consistency

--- routes/api/groups.js
+++ routes/api/groups.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 
 
 const { createGroup, retrieveGroup } = require("../../controllers/groups_controller");
 
 
-router.post('/', passport.authenticate('jwt', {session: false}), createGroup);
-router.get('/:groupId', passport.authenticate('jwt', {session: false}), retrieveGroup);
+router.post("/", passport.authenticate("jwt", {session: false}), createGroup);
+router.get("/:groupId", passport.authenticate("jwt", {session: false}), retrieveGroup);
 
 module.exports = router;
