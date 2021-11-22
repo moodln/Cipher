9bdeb0423152d01f934ed0f01e9357a2d9cabe43
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-22 16:08:35 -0500

    Clean code and implement ES6 syntax

--- frontend/src/components/group_index/group_index.js
+++ frontend/src/components/group_index/group_index.js
@@ -5,64 +5,56 @@ import { selectGroupsWhereCurrentUserParticipant } from '../../selectors/groups_
 import GroupIndexBadgeContainer from './group_index_badge';
 
 class GroupIndex extends Component {
-  constructor(props) {
-    super(props);
-
-
-  }
-  componentDidMount() {
-    console.log('mounting!!');
-    
-    this.props.fetchUserGroups();
-  }
-
-
-  render() {
-    if (this.props.groups.length === 0) {
-      return (
-        <section className="problem-index-problems-section">
-          <div className="problem-index-problems-list-div">
-            <div className="problem-index-problems-header-div">
-              <h1>You have not joined or created any groups yet!</h1>
-            </div>
-          </div>
-        </section>
-      )
-    } else {
-      return (
-        <section className="problem-index-problems-section">
-          <div className="problem-index-problems-list-div">
-            <div className="problem-index-problems-header-div">
-              <h1>Your Groups:</h1>
-            </div>
-            <div className="problem-index-problems-div">
-              <ul className="problem-index-problems-list">
-                {
-                  this.props.groups.map(group => {
-                    return (
-                      <GroupIndexBadgeContainer key={group["_id"]} group={group} />
-                    )
-                  })
-                }
-              </ul>
-            </div>
-          </div>
-        </section>
-      );
+    componentDidMount() {
+        this.props.fetchUserGroups();
     }
-  }
+
+    render() {
+        if (this.props.groups.length === 0) {
+            return (
+                <section className="problem-index-problems-section">
+                    <div className="problem-index-problems-list-div">
+                        <div className="problem-index-problems-header-div">
+                            <h1>You have not joined or created any groups yet!</h1>
+                        </div>
+                    </div>
+                </section>
+            )
+        }
+        else {
+            return (
+                <section className="problem-index-problems-section">
+                    <div className="problem-index-problems-list-div">
+                        <div className="problem-index-problems-header-div">
+                            <h1>Your Groups:</h1>
+                        </div>
+                        <div className="problem-index-problems-div">
+                            <ul className="problem-index-problems-list">
+                                {
+                                    this.props.groups.map(group => (
+                                        <GroupIndexBadgeContainer
+                                            key={group["_id"]}
+                                            group={group} />
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+            );
+        }
+    }
 }
 
-
-const mapStateToProps = (state) => {
-
-  return {
-    groups: selectGroupsWhereCurrentUserParticipant(Object.values(state.entities.groups.byId), state.session.user.id),
-  }
-}
+const mapStateToProps = state => ({
+        groups: selectGroupsWhereCurrentUserParticipant(
+            Object.values(state.entities.groups.byId),
+            state.session.user.id
+        )
+});
 
 const mapDispatchToProps = dispatch => ({
-  fetchUserGroups: () => dispatch(fetchCurrentUserGroups())
-})
+    fetchUserGroups: () => dispatch(fetchCurrentUserGroups())
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(GroupIndex)
+export default connect(mapStateToProps, mapDispatchToProps)(GroupIndex);
