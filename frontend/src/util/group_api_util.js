88742b8ab66573fffa9c26da8698cf3d15aaceac
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-22 16:00:16 -0500

    Clean code and implement ES6 syntax

--- frontend/src/util/group_api_util.js
+++ frontend/src/util/group_api_util.js
@@ -1,19 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const fetchCurrentUserGroups = () => {
-  return axios.get('/api/users/groups');
+    return axios.get("/api/users/groups");
 };
 
 export const createGroupWithProblem = (problemId) => {
-  return axios.post('/api/groups/', { headers: { problemId } });
-}
+    return axios.post("/api/groups/", { headers: { problemId } });
+};
 
 export const removeCurrentUserFromGroup = (groupId) => {
-  return axios.delete(`/api/users/${groupId}`);
-}
+    return axios.delete(`/api/users/${groupId}`);
+};
 
 export const fetchGroup = (groupId) => {
-  // console.log(`groupId: `, groupId);
-
-  return axios.get(`/api/groups/${groupId}`);
-}
+    return axios.get(`/api/groups/${groupId}`);
+};
