747484f9e8b67d2f35baec100e9e9a68551ab65e
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-22 16:09:37 -0500

    Clean code and implement ES6 syntax

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -1,18 +1,13 @@
-import * as UserApiUtil from "../util/user_api_uitl"
+import * as UserApiUtil from "../util/user_api_util";
 
-export const RECEIVE_USERS_SEARCH = 'RECEIVE_USERS_SEARCH';
+export const RECEIVE_USERS_SEARCH = "RECEIVE_USERS_SEARCH";
 
-export const receiveUsersSearch = (usersCollection) => {
-  return {
+export const receiveUsersSearch = usersCollection => ({
     type: RECEIVE_USERS_SEARCH,
     usersCollection
-  }
-}
+});
 
-export const fetchUsersToInvite = (usersInGroup) => dispatch => {
-  return UserApiUtil.fetchUsersToInvite(usersInGroup)
-  .then( usersResponse => {
-    
-    return dispatch(receiveUsersSearch(usersResponse.data))
-  })
-}
+export const fetchUsersToInvite = usersInGroup => dispatch => {
+    return UserApiUtil.fetchUsersToInvite(usersInGroup)
+        .then(usersResponse => dispatch(receiveUsersSearch(usersResponse.data)))
+};
