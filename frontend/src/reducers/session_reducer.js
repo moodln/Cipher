0d2f3a169d6ac62fb7e28ed6d7bbe7a0546d567c
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-22 16:05:29 -0500

    Clean code and implement ES6 syntax

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,32 +1,34 @@
-import { RECEIVE_CURRENT_USER, REMOVE_CURRENT_USER, RECEIVE_USER_SIGN_IN } from '../actions/session_actions';
+import {
+    RECEIVE_CURRENT_USER,
+    REMOVE_CURRENT_USER,
+    RECEIVE_USER_SIGN_IN
+} from '../actions/session_actions';
 
 const initialState = {
-  isAuthenticated: false,
-  user: {}
+    isAuthenticated: false,
+    user: {}
 };
 
-const sessionReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case REMOVE_CURRENT_USER:
-      return {
-        isAuthenticated: false,
-        user: undefined
-      };
-    case RECEIVE_CURRENT_USER:
-      
-      return {
-        isAuthenticated: !!action.currentUser,
-        user: action.currentUser
-      };
-    case RECEIVE_USER_SIGN_IN:
-      
-      return {
-        isAuthenticated: !!action.currentUser,
-        user: action.currentUser
-      };
-    default:
-      return state;
-  }
-}
+const SessionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case REMOVE_CURRENT_USER:
+            return {
+                isAuthenticated: false,
+                user: undefined
+            };
+        case RECEIVE_CURRENT_USER:
+            return {
+                isAuthenticated: !!action.currentUser,
+                user: action.currentUser
+            };
+        case RECEIVE_USER_SIGN_IN:
+            return {
+                isAuthenticated: !!action.currentUser,
+                user: action.currentUser
+            };
+        default:
+            return state;
+    }
+};
 
-export default sessionReducer;
+export default SessionReducer;
