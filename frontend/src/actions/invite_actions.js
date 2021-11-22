8696a0eabdd9c7806fcc5d01940c1346ef2194f8
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-22 16:09:22 -0500

    Clean code and implement ES6 syntax

--- frontend/src/actions/invite_actions.js
+++ frontend/src/actions/invite_actions.js
@@ -1,53 +1,38 @@
 import * as InviteApiUtil from "../util/invite_api_util"
-import { receiveGroup } from "./group_actions";
 
-export const RECEIVE_INVITE = 'RECEIVE_INVITE';
-export const RECEIVE_INVITES = 'RECEIVE_INVITES';
-export const REMOVE_INVITE = 'REMOVE_INVITE';
+export const RECEIVE_INVITE = "RECEIVE_INVITE";
+export const RECEIVE_INVITES = "RECEIVE_INVITES";
+export const REMOVE_INVITE = "REMOVE_INVITE";
 
-export const receiveInvite = (invite) => {
-  return {
+export const receiveInvite = invite => ({
     type: RECEIVE_INVITE,
     invite
-  }
-}
+});
 
-export const receiveInvites = (invitesCollection) => {
-  return {
+export const receiveInvites = invitesCollection => ({
     type: RECEIVE_INVITES,
     invitesCollection
-  }
-}
+});
 
-export const removeInvite = (inviteId, resolutionCollection) => {
-  return {
+export const removeInvite = (inviteId, resolutionCollection) => ({
     type: REMOVE_INVITE,
     inviteId,
     resolutionCollection
-  }
-}
+});
 
 export const inviteUserToGroup = (inviteeId, groupId) => dispatch => {
-  return InviteApiUtil.inviteUserToGroup(inviteeId, groupId)
-  .then( inviteResponse => {
-    
-    return dispatch(receiveInvite(inviteResponse.data))
-  })
-}
+    return InviteApiUtil.inviteUserToGroup(inviteeId, groupId)
+        .then(inviteResponse => dispatch(receiveInvite(inviteResponse.data)))
+};
 
 export const fetchCurrentUserInvites = () => dispatch => {
-  return InviteApiUtil.fetchCurrentUserInvites()
-  .then( invitesResponse => {
-    
-    return dispatch(receiveInvites(invitesResponse.data))
-  })
-}
+    return InviteApiUtil.fetchCurrentUserInvites()
+        .then(invitesResponse => dispatch(receiveInvites(invitesResponse.data)))
+};
 
 export const inviteResolution = (inviteId, response) => dispatch => {
-  return InviteApiUtil.inviteResolution(inviteId, response)
-  .then( (resolutionResponse) => {
-    
-    return dispatch(removeInvite(inviteId, resolutionResponse.data))
-  })
-}
-
+    return InviteApiUtil.inviteResolution(inviteId, response)
+        .then(resolutionResponse => {
+            return dispatch(removeInvite(inviteId, resolutionResponse.data));
+        })
+};
