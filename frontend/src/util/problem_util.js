2b3041a2886b89a55d9c4b32f91d778c850fac0b
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-22 15:59:10 -0500

    Delete files and replace with new names

--- frontend/src/util/problem_util.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const fetchAllProblems = () => {
-    return axios.get('/api/problems/')
-}
-
-export const fetchProblem = id => {
-    return axios.get(`/api/problems/${id}`)
-}
