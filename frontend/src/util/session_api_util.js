653bec767d2f4c81fc2049b8e0a1a865c6417d54
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-22 15:59:33 -0500

    Clean code and implement ES6 syntax

--- frontend/src/util/session_api_util.js
+++ frontend/src/util/session_api_util.js
@@ -1,17 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const setAuthToken = token => {
-  if (token) {
-    axios.defaults.headers.common['Authorization'] = token;
-  } else {
-    delete axios.defaults.headers.common['Authorization'];
-  }
+    if (token) {
+        axios.defaults.headers.common["Authorization"] = token;
+    }
+    else {
+        delete axios.defaults.headers.common["Authorization"];
+    }
 };
 
 export const createUser = user => {
-  return axios.post('/api/users/register', user);
+    return axios.post("/api/users/register", user);
 };
 
 export const createSession = user => {
-  return axios.post('/api/users/login', user);
+    return axios.post("/api/users/login", user);
 };
