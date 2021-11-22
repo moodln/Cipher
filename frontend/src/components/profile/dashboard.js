9a335b69d7445dfc64eca59f5e77034c84040859
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-22 16:07:45 -0500

    Clean code and implement ES6 syntax

--- frontend/src/components/profile/dashboard.js
+++ frontend/src/components/profile/dashboard.js
@@ -3,37 +3,24 @@ import { connect } from 'react-redux';
 import GroupIndexContainer from '../group_index/group_index';
 import SidebarContainer from "../sidebar/sidebar_container";
 
-
-class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="page-with-sidebar">
+const Dashboard = props => (
+    <div className="page-with-sidebar">
         <SidebarContainer />
         <div className="problem-index container">
-          <section className="problem-index-header">
-            <div className="problem-index-header-div">
-              <div className="problem-intro">
-                <h1>Welcome, {this.props.currentUser.handle}</h1>
-              </div>
-            </div>
-          </section>
-          <GroupIndexContainer />
+            <section className="problem-index-header">
+                <div className="problem-index-header-div">
+                    <div className="problem-intro">
+                        <h1>Welcome, {props.currentUser.handle}</h1>
+                    </div>
+                </div>
+            </section>
+            <GroupIndexContainer />
         </div>
-      </div>
-    )
-  }
-}
+    </div>
+)
 
 const mapStateToProps = (state) => ({
-  currentUser: state.session.user
+    currentUser: state.session.user
 });
 
-const mapDispatchToProps = dispatch => ({
-
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default connect(mapStateToProps)(Dashboard);
