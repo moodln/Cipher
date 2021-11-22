c862f5fe3f09ce7b5939dbdc2a4a94a6595b562a
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-22 16:00:07 -0500

    Clean code and implement ES6 syntax

--- frontend/src/util/invite_api_util.js
+++ frontend/src/util/invite_api_util.js
@@ -1,14 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const fetchCurrentUserInvites = () => {
-  return axios.get('/api/users/invites');
+    return axios.get("/api/users/invites");
 };
 
 export const inviteUserToGroup = (inviteeId, groupId) => {
-  // console.log(`inviteeId: `, inviteeId);
-  return axios.post('/api/invites/', { headers: { inviteeId, groupId } });
-}
+    return axios.post("/api/invites/", { headers: { inviteeId, groupId } });
+};
 
 export const inviteResolution = (inviteId, response) => {
-  return axios.delete(`/api/invites/${inviteId}`, { headers: { response } });
-}
+    return axios.delete(`/api/invites/${inviteId}`, { headers: { response } });
+};
