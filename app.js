d1d2bb5b0519e8c3f4e9a630256d00c4e5ee1057
Arebiter <pasandharmasena@gmail.com>
2021-11-29 15:17:00 -0500

    remove path from express peer server

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const io = require("socket.io")(httpServer, {
 });
 const peerServer = ExpressPeerServer(httpServer, {
   debug: true,
-  path: "/peerjs"
+  path: "/"
 });
 app.use("/peerjs", peerServer);
 
