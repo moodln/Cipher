bc46352fad4463c69a071a754d430daab60c657f
mwilson <mjwilsonmd@gmail.com>
2021-12-02 07:55:44 -0500

    add color class to notfication div

--- frontend/src/components/ui_components/user_notifications.js
+++ frontend/src/components/ui_components/user_notifications.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { connect } from "react-redux"
 import { fetchCurrentUserInvites } from "../../actions/invite_actions";
+import { selectCurrentUserInvites } from "../../selectors/invites_selector"
 import UserInvitationsContainer from "./user_invitations";
 
 export class UserNotifications extends React.Component {
@@ -11,14 +12,27 @@ export class UserNotifications extends React.Component {
         }
     }
 
+    componentDidMount() {
+        this.props.fetchUserInvites();
+    }
+
     render() {
+        let color;
+        if (this.props.invites.length === 0) {
+            color = '';
+        } else {
+            color = 'color';
+        }
+        //// onMouseEnter={() => this.props.fetchUserInvites()}>
+        debugger
+        //fill="currentColor"
         return (
             <div onClick={() => this.setState({ displayUserInvites: !this.state.displayUserInvites })}>
                 <div className="notification-badge">
-                    <div className="notification-img"
-                        onMouseEnter={() => this.props.fetchUserInvites()}>
+                    <div className={`notification-img ${color}`}>
+                        
                         <svg xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor"
+                            
                             className="bi bi-bell"
                             viewBox="0 0 16 16">
                             <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
@@ -32,8 +46,15 @@ export class UserNotifications extends React.Component {
         )
     }
 }
+const mapStateToProps = state => ({
+        invites: selectCurrentUserInvites(
+            state.entities.invites.byId,
+            state.session.user.id
+    )
+})
+
 const mapDispatchToProps = dispatch => ({
     fetchUserInvites: () => dispatch(fetchCurrentUserInvites())
 });
 
-export default connect(null, mapDispatchToProps)(UserNotifications);
+export default connect(mapStateToProps, mapDispatchToProps)(UserNotifications);
