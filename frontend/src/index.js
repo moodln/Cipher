d6a3d89b531b0e7f0dff60e719e48e44f9dac74b
Arebiter <pasandharmasena@gmail.com>
2021-12-01 21:51:47 -0500

    Remove login from index file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,10 +15,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
         store = configureStore(preloadedState);
         const currentTime = Date.now() / 1000;
-        
+
         if (decodedUser.exp < currentTime) { // If the user's token has expired
             store.dispatch(logout());
-            window.location.href = "/login";
+            window.location.href = "/";
         }
     }
     else { // If this is a first time user, start with an empty store
